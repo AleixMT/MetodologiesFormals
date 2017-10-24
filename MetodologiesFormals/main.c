5fc82962d37d5c72b16d3f651d23ff5cc9e2faae
toful <tofuldauden@gmail.com>
2017-10-24 09:12:35 +0200

    Tòful: primer commit, definida l'estructuta i variables de treball

---
+++ MetodologiesFormals/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N_MAX 10
+
+typedef struct{
+    unsigned int n;
+    int elems[N_MAX];
+}t_llista;
+
+int main()
+{
+    t_llista ll;
+    int e;
+
+
+    return 0;
+}
